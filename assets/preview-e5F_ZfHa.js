34952cf2cf7e40d12a8e9a54134448a8e7acb077
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2024-06-19 22:42:12 -0300

    Updates

---
+++ assets/preview-e5F_ZfHa.js
@@ -0,0 +1,84 @@
+import{h as r,j as o}from"./styled-components.browser.esm-CK3y3u_d.js";import"./index-CEThVCg_.js";const a=r`
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  :root {
+    --main-bg-color: #282828;
+    --main-fg-color: #d5c4a1;
+    --main-fg-reverse-color: #282828;
+    --color-primary: #83a598;
+    --color-secondary: #fabd2f;
+    --color-support: #b8bb26;
+    --color-extra: #d65d0e;
+    --text-primary: #8ec07c;
+    --text-comment: #a89984;
+    --visual-selection: #458588;
+    --visual-ui1: #3c3836;
+    --visual-ui2: #504945;
+    --status-warning: #b8bb26;
+    --status-error: #cc241d;
+    --status-success: #689d6a;
+    --status-info: #83a598;
+    --status-danger: #fb4934;
+  }
+
+  body {
+    font-family: 'Open Sans Condensed';
+    background-color: var(--main-bg-color);
+    color: var(--main-fg-color);
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--color-primary);
+  }
+
+  button {
+    font-family: 'Open Sans Condensed';
+    cursor: pointer;
+    color: var(--main-fg-reverse-color);
+    background-color: var(--color-primary);
+    border: 2px solid var(--visual-ui1);
+  }
+`,i={decorators:[e=>o.jsxs(o.Fragment,{children:[o.jsx(a,{}),o.jsx(e,{})]})]};export{i as default};
