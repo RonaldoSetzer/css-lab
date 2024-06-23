e8c404f881b1f9a99c6e349dcc87a44b3f4f76f8
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2024-06-22 23:04:46 -0300

    feat: add HorizontalNavMenu component

---
+++ src/components/navigations/horizontal-nav-menu.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Navigation from './horizontal-nav-menu';
+
+const meta = {
+  title: 'Navigation/ Horizontal Navigation',
+  component: Navigation,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Navigation>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = { };
+
+
+
---
+++ src/components/navigations/horizontal-nav-menu.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+const List = styled.ul`
+  display: flex;
+  list-style-type: none;
+  padding: 0;
+  background-color: #333;
+`;
+
+const ListItem = styled.li`
+  flex: 1;
+`;
+
+const Link = styled.a`
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+
+  &:hover {
+    background-color: #111;
+  }
+`;
+
+function HorizontalNavigation() {
+  return (
+    <List>
+      <ListItem><Link href="/home">Home</Link></ListItem>
+      <ListItem><Link href="/about">About</Link></ListItem>
+      <ListItem><Link href="/contact">Contact</Link></ListItem>
+    </List>
+  );
+}
+
+export default HorizontalNavigation;
