865dc4842fd0eb7a13fbba112af35218787d2e99
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2024-06-27 21:51:01 -0300

    feat: add ui Button component

--- src/components/ui.tsx
+++ src/components/ui.tsx
@@ -14,3 +14,18 @@ export const LinkButton = styled.a`
   border-radius: 25px;
   cursor: pointer;
 `;
+
+export const Button = styled.button`
+  font-family: 'Arial';
+  background-color: var(--color-primary);
+  border: 2px solid var(--visual-ui2);
+  color: var(--main-bg-color);
+  padding: 12px 24px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 22px;
+  font-weight: 600;
+  border-radius: 25px;
+  cursor: pointer;
+`;
